86c188e4bce41ae09b0aad04ff3f7ccbed39b3d9
Jonathan Bonenfant <bonenfantjo97@gmail.com>
2021-10-04 16:23:15 -0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 (function() {
 
     let passReg = /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{12,}$/,
-        elInput = document.querySelector(['data-js-input']);
+        elInput = document.querySelector(['data-js-input="password"']);
 
         console.log(elInput);
 
 
-    if (elInput.dataset == 'password') {
+    if (elInput.dataset.jsInput == 'password') {
                 console.log(elInputs.value);
             }
 
