f035bbd7e02e3360f219138bc94e83f385e4bfc4
Jonathan Bonenfant <bonenfantjo97@gmail.com>
2021-10-04 16:10:26 -0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 (function() {
 
     let passReg = /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{12,}$/,
-        passDom = document.querySelector(['input']);
+        passDom = document.querySelectorAll(['input']);
 
         console.log(passDom);
 
