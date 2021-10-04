3873aa839abd180ad5ff20d530e6c5be5f04b220
Jonathan Bonenfant <bonenfantjo97@gmail.com>
2021-10-04 16:41:03 -0400

    jsfinal

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 (function() {
 
     let passReg = /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{12,}$/,
-        elInput = document.querySelectorAll(['input']);
+        elInput = document.querySelectorAll(['input']),
+        isValid = true;
 
         console.log(elInput);
 
@@ -10,7 +11,16 @@
         console.log(elInput[i].value);
 
         if (elInput[i].dataset == "password") {
-                console.log(elInputs.value);
+                console.log(elInput.value);
+
+                if (passReg.test(elInput.value)) {
+
+                }
+                else if (elInput[i].required && elInput[i].value == '') {
+
+                    isValid = false
+                    elInput[i].classList.add('error');
+                }
             }
 
         }
